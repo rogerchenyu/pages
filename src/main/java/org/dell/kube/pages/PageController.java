405540ce40d9d815f980de50c6e82f179b792de9
Roger Chen <rogerchenyu@gmail.com>
2020-11-26 01:51:25 +0000

    Add log configurations

--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -1,4 +1,62 @@
 package org.dell.kube.pages;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 
+@RestController
+@RequestMapping("/pages")
 public class PageController {
+    Logger logger =(Logger)LoggerFactory.getLogger(this.getClass());
+    private IPageRepository pageRepository;
+    public PageController(IPageRepository pageRepository)
+    {
+        this.pageRepository = pageRepository;
+    }
+    @PostMapping
+    public ResponseEntity<Page> create(@RequestBody Page page) {
+
+        logger.info("CREATE-INFO:Creating a new record");
+        logger.debug("CREATE-DEBUG:Creating a new record");
+
+        Page newPage= pageRepository.create(page);
+
+        logger.info("CREATE-INFO:Created successfully");
+        logger.debug("CREATE-DEBUG:Created successfully");
+        return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Page> read(@PathVariable long id) {
+
+        logger.info("READ-INFO:Fetching page with id = " + id);
+        logger.debug("READ-DEBUG:Fetching page with id = " + id);
+
+
+        Page page = pageRepository.read(id);
+        if(page!=null)
+            return new ResponseEntity<Page>(page, HttpStatus.OK);
+        else
+            logger.error("READ-ERROR:Could not find page with id = " + id);
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+    @GetMapping
+    public ResponseEntity<List<Page>> list() {
+        List<Page> pages= pageRepository.list();
+        return new ResponseEntity<List<Page>>(pages, HttpStatus.OK);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<Page> update(@RequestBody Page page, @PathVariable long id) {
+        Page updatedPage= pageRepository.update(page,id);
+        if(updatedPage!=null)
+            return new ResponseEntity<Page>(updatedPage, HttpStatus.OK);
+        else
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable long id) {
+        pageRepository.delete(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 }
